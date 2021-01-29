aca244cf7c15d2970bf8e80f278d081cbd00ecbf
LucasDos96 <dossantos.lucas12@gmail.com>
2021-01-29 10:13:53 +0100

    logo flags

--- utils/src/header.php
+++ utils/src/header.php
@@ -4,24 +4,15 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
         </button>
     </div>
     <div class="collapse navbar-collapse">
         <ul class="nav navbar-nav">
-            <li> <a href="Depot.php"><span class="glyphicon glyphicon-home"></span> Depot</a> </li>
-            <li>
-                <a href="Cours.php">
-                    <span class="glyphicon glyphicon-search"></span> Cours
-                </a>
-            </li>
-            <li> <a href="?language=FR"> <img src="/utils/images/french-flag.svg"> </a> </li>
-            <!--<li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown"> Langue <span class="caret"></span></a>
-                <ul class="dropdown-menu" role="menu">
-                    <li><a href="?language=FR">Français</a></li>
-                    <li><a href="?language=EN">Anglais</a></li>
-                </ul>
-            </li>-->
+            <li><a href="Depot.php"><span class="glyphicon glyphicon-home"></span> Depot </a></li>
+            <li><a href="Cours.php"> <span class="glyphicon glyphicon-search"></span> Cours </a></li>
+            <li><a href="?language=FR"><img alt="Français" title="Français" src="/CCES/utils/images/french-flag.svg" style="width: 20px;"></a></li>
+            <li><a href="?language=EN"><img alt="English" title="English" src="/CCES/utils/images/english-flag.svg" style="width: 20px;"></a></li>
         </ul>
         <div class="form-group navbar-right navbar-form">
             <button type="submit" onclick="goCoursPage()" class="btn btn-info" target="_blank">Vue étudiante actuelle</button>
